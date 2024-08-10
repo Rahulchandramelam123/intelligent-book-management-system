35f59fc3482a5af20077cae0a0505559467a5aec
Melam Chandu <150601451+Rahulchandramelam123@users.noreply.github.com>
2024-08-10 14:30:44 +0530

    Update config.py

--- book_management/config.py
+++ book_management/config.py
@@ -2,4 +2,4 @@ import os
 
 class Config:
     SQLALCHEMY_DATABASE_URI = """postgresql://postgres:DMksFid2Qg77iOCD7M22@database-1.cxs6uyymyn63.ap-southeast-2.rds.amazonaws.com:5432/
-database-1"""
+postgres"""
