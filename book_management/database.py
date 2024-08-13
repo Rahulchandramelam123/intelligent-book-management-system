f1e6916bed7c2fcd840274c2690066b430d9b130
Melam Chandu <150601451+Rahulchandramelam123@users.noreply.github.com>
2024-08-13 17:54:31 +0530

    Update database.py

--- book_management/database.py
+++ book_management/database.py
@@ -1,3 +1,16 @@
-from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+from sqlalchemy.orm import sessionmaker
+from book_management.config import Config
 
-db = SQLAlchemy()
+DATABASE_URL = Config.SQLALCHEMY_DATABASE_URI
+
+# Create an asynchronous engine
+engine = create_async_engine(DATABASE_URL, echo=True)
+
+# Create a configured "Session" class
+AsyncSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine, class_=AsyncSession)
+
+# Dependency
+async def get_db():
+    async with AsyncSessionLocal() as session:
+        yield session
